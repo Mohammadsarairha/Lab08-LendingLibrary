a4c899db7fb6609c31627cdb2370cc28572abe48
Mohammadsarairha <mohammadsarairha@gmail.com>
2022-04-06 01:06:06 +0300

    Add Lending Library App

---
+++ LendingLibrary/LendingLibrary/Book.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LendingLibrary
+{
+    internal class Book
+    {
+        public string Title { get; set; }
+        public string Author { get; set; }
+
+    }
+}
